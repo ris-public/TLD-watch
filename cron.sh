95d301cf77f0adedc53f79352e49fee35f29304d
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-11-02 21:17:28 +0530

    aha - Ansi HTML Adapter.

--- cron.sh
+++ cron.sh
@@ -2,3 +2,4 @@ curl -s "https://data.iana.org/TLD/tlds-alpha-by-domain.txt" | tail -n +2 >tlds-
 git add tlds-alpha-by-domain.txt
 git commit -m "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
 git push
+git -c color.ui=always log -p --pretty -2 | aha -b >/var/www/html/yamk/index.html
